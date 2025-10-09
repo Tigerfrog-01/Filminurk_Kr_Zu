52f2ae6d7384d64c64cdd869badfe383ef713ebe
Tigerfrog-01 <kristopherzuravljov005@gmail.com>
2025-10-09 11:06:48 +0300

    Movies modeldata for both domain and viewmodels

---
+++ Filminurk/Filminurk.Core/Domain/Movie.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filminurk.Core.Domain
+{
+    public enum Genre
+    {
+
+    }
+
+
+
+    public class Movie
+    {
+        public Guid ID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateOnly FirstPublished { get; set; }
+
+        public string  Director { get; set; }
+
+        public List<string>? Actors { get; set; }
+
+        public decimal? CurrentRating { get; set; }
+
+        //public List<UserComment>? Reviews { get; set; }
+
+        /**/
+
+        /* 3 õpilase valitud andmetüüpi */
+
+        public int IMDBrating { get; set; }
+
+        public Genre Genre { get; set; }
+
+        public int AgeRating { get; set; }
+
+
+    }
+}
---
+++ Filminurk/Filminurk/Models/Movies/MoviesIndexViewModel.cs
@@ -0,0 +1,29 @@
+﻿using Filminurk.Core.Domain;
+
+namespace Filminurk.Models.Movies
+{
+    public class MoviesIndexViewModel
+    {
+        public Guid ID { get; set; }
+
+        public string Title { get; set; }
+
+
+        public DateOnly FirstPublished { get; set; }
+
+        public decimal? CurrentRating { get; set; }
+
+        //public List<UserComment>? Reviews { get; set; }
+
+        /**/
+
+        /* 3 õpilase valitud andmetüüpi */
+
+     
+
+        public string Genre? Genre { get; set; }
+
+        public int AgeRating { get; set; }
+
+    }
+}
