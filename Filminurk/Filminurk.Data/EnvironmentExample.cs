5ccbe534d91ff924f426d8d614ed88541a57e8c4
Tigerfrog-01 <kristopherzuravljov005@gmail.com>
2025-12-05 12:02:05 +0200

    Created EnvironmentExample

---
+++ Filminurk/Filminurk.Data/EnvironmentExample.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Filminurk.Data
+{
+    public class EnvironmentExample //rename class to just Environment"
+    {
+        public static string smtpaddress = ""; //server address for sending emails via smtp
+        public static string gmailpassword = ""; //password for user, that has access to this server (google app password)
+        public static string gmailusername = ""; //username of email sending user(gmail account)
+    }
+}
