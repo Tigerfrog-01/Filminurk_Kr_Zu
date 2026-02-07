53bd2880d86f189b811ff0a2575801fafe0d5879
KŽ <kristopherzuravljov005@gmail.com>
2026-02-07 16:22:05 +0200

    Lisasin kontrolleri, ma loodan ,et see tootab

--- Filminurk/Filminurk/Controllers/ImportController.cs
+++ Filminurk/Filminurk/Controllers/ImportController.cs
@@ -1,22 +1,54 @@
-﻿using Filminurk.Core.ServiceInterface;
+﻿using Filminurk.Core.dto;
+using Filminurk.Core.ServiceInterface;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Filminurk.Controllers
 {
     public class ImportController : Controller
     {
-     
+        private readonly IImportServices _importServices;
 
+      
+        public ImportController(IImportServices importServices)
+        {
+            _importServices = importServices;
+        }
 
+       
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
         }
 
+        
+        [HttpPost]
+        public async Task<IActionResult> Import(string movieTitle)
+        {
+            if (string.IsNullOrEmpty(movieTitle))
+            {
+                ViewBag.Error = "Sisesta filmi pealkiri";
+                return View("Index");
+            }
 
+           
+            var dto = new ImportDTO { Title = movieTitle };
 
+            
+            var result = await _importServices.ImportMovieService(dto);
 
+            if (result != null)
+            {
+                
+                ViewBag.Message = $"Film '{result.Title}' ({result.Year}) Palju onne, film on edukalt valmis!";
+            }
+            else
+            {
+                ViewBag.Error = "Film kas ei eksisteeri voi tekkis sinul midagi error ";
+            }
 
+            return View("Index");
+        }
     }
 }
 
